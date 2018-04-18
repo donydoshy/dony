2f9d52b3380d6ee50976ffd83136cf0cc21c7bf7
DONY DOSHY <48316@fisat>
2018-04-18 04:57:40 -0400

    to add 4 num

--- add.py
+++ add.py
@@ -1,3 +1,13 @@
+def add2(n1, n2):
+			s = n1 + n2
+			return s
+
+		print add2(3, 4)
+def add2(n1, n2,n3):
+			s = n1 + n2+n3
+			return s
+
+		print add2(3, 4,5)
 def add2(n1, n2,n3,n4):
 			s = n1 + n2+n3+n4
 			return s
